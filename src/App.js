24d483995bb6158636f394d9b60f666eb2ffc6ab
bc <benchoe@2six.org>
2022-05-24 17:31:31 -0400

    edit 2

--- src/App.js
+++ src/App.js
@@ -9,8 +9,36 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import axios from 'axios';
+
+let SERVER = process.env.REACT_APP_SERVER;
 
 class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    books: {},
+  }
+}
+
+getBooks = async (booksToRetrieve) => {
+  (await axios.get(`${SERVER}/books`, booksToRetrieve))
+    .then(res => {console.log(res); this.setState({books: res.data});})
+    .catch(err => {res.status(500).send(err);});
+}
+
+createBooks = async (booksToCreate) => {
+  (await axios.post(`${SERVER}/books`, booksToCreate))
+    .then(res => {console.log(res); this.setState({books: res.data});})
+    .catch(err => {res.status(500).send(err);});
+}
+
+deleteBooks = async (bookIdToDelete) =>{
+  (await axios.delete(`${SERVER}/books`, bookIdToDelete))
+    .then(res => { console.log(res); this.setState({books: this.state.books.filter(stateBooks => stateBooks._id !== bookIdToDelete)})})
+    .catch(err => {res.status(500).send(err);});
+}
+
   render() {
     return (
       <>
@@ -18,7 +46,7 @@ class App extends React.Component {
           <Header />
           <Switch>
             <Route exact path="/">
-              <BestBooks/>
+              { this.state.books && <BestBooks onBookDelete={this.handleDeleteBooks} books={this.state.books}/> }
             </Route>
             <Route path="/about">
               <About />
