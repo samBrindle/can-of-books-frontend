0c5a9b9280c62b5eac2c1473fc5d078f55136a97
bc <benchoe@2six.org>
2022-05-24 20:04:20 -0400

    working

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,76 +1,92 @@
 import React from 'react';
-import axios from 'axios';
-import { Carousel, Container, ListGroup } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
 require('dotenv').config();
 
-
-
 export default class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
-  
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
   render() {
-
-    /* TODO: render all the books in a Carousel */
-    // let book = this.state.books;
-
-    let bookArr = this.state.books.map((story, idx) => {
-      return (
-        <Carousel.Item key={idx} >
-          <img
-            src="https://via.placeholder.com/150"
-            alt={story.title}
-          />
-          <Carousel.Caption>
-            <h3>{story.title}</h3>
-            <p>Description: {story.description}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      )
-    })
-
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length} ? (
-
-        <Container>
-          <Carousel>
-            {bookArr}
-          </Carousel>
-        </Container>
-
-        <p>Book Carousel coming soon</p>
-        ) : (
-        <h3>No Books Found :(</h3>
-        )
+        <ListGroup>
+          {this.props.books.length && this.props.books.map(book => (
+            <BestBook book={book} onBookDelete={this.props.onBookDelete} />
+          ))}
+        </ListGroup>
+        <NewBook onBookCreate={this.props.onBookCreate} />
       </>
     )
   }
 }
 
 class BestBook extends React.Component {
+  render() {
+    return (
+      <ListGroup.Item key={this.props.book._id}>{this.props.book.title}
+        <button onClick={this.props.onBookDelete(this.props.book._id)}>Delete</button>
+      </ListGroup.Item>
+    )
+  }
+}
+
+class NewBook extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      book = this.props.book
+      title: undefined,
+      description: undefined,
+      status: undefined
     };
   }
-  render () {
+  handleTitleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+    console.log(this.state);
+  }
+  handleDescriptionChange = (e) => {
+    this.setState({
+      description: e.target.value,
+    });
+    console.log(this.state);
+  } 
+  handleStatusChange = (e) => {
+    this.setState({
+      status: e.target.checked
+    });
+    console.log(this.state);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onBookCreate(this.state);
+  }
+
+  render() {
     return (
-      <ListGroup.Item>{book.title}
-        <Button 
-          variant="danger"
-          type="button"
-        >Delete</Button>
-      </ListGroup.Item>
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group className="mb-3" >
+          <Form.Label>Title</Form.Label>
+          <Form.Control type="text" placeholder="Enter Title" name="title" onChange={this.handleTitleChange} />
+          <Form.Text className="text-muted">
+            We'll always sell your information.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" >
+          <Form.Label>Description</Form.Label>
+          <Form.Control type="text" placeholder="Enter Description" name="description" onChange={this.handleDescriptionChange}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Did you do your homework?" ref="status" onChange={this.handleStatusChange} />
+        </Form.Group>
+        <Button
+          variant="secondary"
+          type="submit"
+        >
+          Add New
+        </Button>
+      </Form>
     )
   }
 }
